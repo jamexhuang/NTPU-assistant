3e5e9553bdb85d264c7d08bbe3ac270583d0ea5f
jamexhuang <jamexhuang@gmail.com>
2024-04-29 21:03:44 +0800

    Update date and time variables in main.js

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-// 設定月份變數
-const monthList = "2024-04";
-
 // 設定序號變數，計劃名稱右邊的數字
 const thisSeqID = "67028";
 
+// 設定年月份變數
+const monthList = "2024-04";
+
 // 設定日期列表
-const dates = ["05"];
+const dates = ["06"];
 
-// 設定開始和結束時間變數
+// 設定每天開始和結束時間變數，24小時制
 const startTime = 09;
-const endTime = 17;
+const endTime = 16;
 
-//********以下都不用改********//
+// 設定隨機分鐘數範圍 (例如，產生 0 到 20 之間的隨機分鐘數)
+const minMinute = 20;
+const maxMinute = 35;
 
 // 工作備註
 const workMemo = "%E6%8E%A5%E8%81%BD%E9%9B%BB%E8%A9%B1%E3%80%81%E7%99%BB%E6%89%93%E5%85%AC%E6%96%87%E3%80%81%E6%89%93%E6%8E%83%E8%BE%A6%E5%85%AC%E5%AE%A4";
@@ -32,8 +34,8 @@ async function processDateRange() {
   const { viewState, viewStateGenerator } = await getViewStateValues();
 
   dates.forEach(date => {
-    // 產生隨機分鐘數 (0-10)
-    const randomMinute = Math.floor(Math.random() * 11).toString().padStart(2, '0');
+    // 產生指定範圍內的隨機分鐘數
+    const randomMinute = (Math.floor(Math.random() * (maxMinute - minMinute + 1)) + minMinute).toString().padStart(2, '0');
 
     // 組合工作開始和結束時間
     const workStart = `${startTime}:${randomMinute}`;
